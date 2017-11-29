6f6992d346adc061d932248f48e404a0ec1fe5c5
Kevin Matthews <kmatthews@cyberdata.net>
2017-11-29 15:57:47 -0800

    Refactor aes_encrypt, add documentation

--- src/main.c
+++ src/main.c
@@ -21,6 +21,14 @@ int aes_init(unsigned char *key_data, int key_data_len, unsigned char *salt,
         return -1;
     }
 
+    /*
+     *  int EVP_EncryptInit_ex(EVP_CIPHER_CTX *ctx, const EVP_CIPHER *type,
+     *      ENGINE *impl, unsigned char *key, unsigned char *iv);
+     *
+     * Set impl to NULL to use the default implementation.
+     * key is the symmetric key
+     * iv is the IV (Initialization vector)
+     */
     EVP_CIPHER_CTX_init(e_ctx);
     EVP_EncryptInit_ex(e_ctx, EVP_aes_128_cbc(), NULL, key, iv);
     EVP_CIPHER_CTX_init(d_ctx);
@@ -29,29 +37,54 @@ int aes_init(unsigned char *key_data, int key_data_len, unsigned char *salt,
     return 0;
 }
 
-unsigned char *aes_encrypt(EVP_CIPHER_CTX *e, unsigned char *plaintext, int *len)
+unsigned char *aes_encrypt(EVP_CIPHER_CTX *e_ctx, unsigned char *plaintext, int *plaintext_len)
 {
-    int c_len = *len + AES_BLOCK_SIZE, f_len = 0;
-    unsigned char *ciphertext = malloc(c_len);
+    // The resulting cipher text can range from 0 bytes to: input_length + cipher_block_size - 1
+    // (not including the null terminator?).
+    int ciphertext_max_len = *plaintext_len + AES_BLOCK_SIZE;
+    int update_encrypt_len = 0, final_encrypt_len = 0;
+    unsigned char *ciphertext = malloc(ciphertext_max_len);
+    unsigned char *ptr = NULL;
 
-    EVP_EncryptInit_ex(e, NULL, NULL, NULL, NULL);
+    // Not sure why, but this allows us to use the same context for multiple encryption cycles
+    EVP_EncryptInit_ex(e_ctx, NULL, NULL, NULL, NULL);
 
-    EVP_EncryptUpdate(e, ciphertext, &c_len, plaintext, *len);
+    /*
+     *  int EVP_EncryptUpdate(EVP_CIPHER_CTX *ctx, unsigned char *out,
+     *      int *outl, unsigned char *in, int inl);
+     *
+     * Encrypt 'in' and place it in 'out'.
+     * Operates on 'inl' (input length) bytes and updates 'outl' accordingly
+     */
+    EVP_EncryptUpdate(e_ctx, ciphertext, &update_encrypt_len, plaintext, *plaintext_len);
+    printf("encrypt update: %d\n", update_encrypt_len);
 
-    EVP_EncryptFinal_ex(e, ciphertext+c_len, &f_len);
+    /*
+     *  int EVP_EncryptFinal_ex(EVP_CIPHER_CTX *ctx, unsigned char *out,
+     *      int *outl);
+     *
+     * If padding is enabled, this encrypts any data that remains in a partial block.
+     * Data is written to out.
+     * The number of bytes written is placed in outl.
+     */
+    // Skip the portion of the buffer that was written to durign the 'Update' process
+    ptr = ciphertext+update_encrypt_len;
+    EVP_EncryptFinal_ex(e_ctx, ptr, &final_encrypt_len);
+    printf("encrypt update: %d\n", final_encrypt_len);
 
-    *len = c_len + f_len;
+    *plaintext_len = update_encrypt_len + final_encrypt_len;
     return ciphertext;
 }
 
-unsigned char *aes_decrypt(EVP_CIPHER_CTX *e, unsigned char *ciphertext, int *len)
+unsigned char *aes_decrypt(EVP_CIPHER_CTX *d_ctx, unsigned char *ciphertext, int *len)
 {
     int p_len = *len, f_len = 0;
     unsigned char *plaintext = malloc(p_len);
 
-    EVP_DecryptInit_ex(e, NULL, NULL, NULL, NULL);
-    EVP_DecryptUpdate(e, plaintext, &p_len, ciphertext, *len);
-    EVP_DecryptFinal_ex(e, plaintext+p_len, &f_len);
+    // Not sure why, but this allows us to use the same context for multiple dencryption cycles
+    EVP_DecryptInit_ex(d_ctx, NULL, NULL, NULL, NULL);
+    EVP_DecryptUpdate(d_ctx, plaintext, &p_len, ciphertext, *len);
+    EVP_DecryptFinal_ex(d_ctx, plaintext+p_len, &f_len);
 
     *len = p_len + f_len;
     return plaintext;
@@ -64,7 +97,7 @@ int main(int argc, char **argv)
     unsigned int salt[] = {12345, 54321};
     unsigned char *key_data;
     int key_data_len, i;
-    char *input[] = {"a", "abcd", NULL};
+    char *input[] = {"a", "abcd", "this is a very long string!! It is so long. So very long.", NULL};
 
     if (argc <= 1)
     {
